f51ee3189c8da0f2d16548396e4bb6b494c9f38a
Piotr Synowiec <psynowiec@gmail.com>
2016-10-17 12:17:53 +0200

    update

--- src/Shop/Product.php
+++ src/Shop/Product.php
@@ -2,12 +2,8 @@
 
 namespace Shop;
 
-<<<<<<< HEAD
 class Product
 {
-=======
-class Product {
->>>>>>> e212dda92d6d7c5779edf86e401122f4813e3bc8
 
     private $id;
     private $name;
@@ -15,12 +11,8 @@ class Product {
     private $price;
     private $description;
 
-<<<<<<< HEAD
     public function __construct($name, $stock, $price, $description, $id = null)
     {
-=======
-    public function __construct($name, $stock, $price, $description, $id = null) {
->>>>>>> e212dda92d6d7c5779edf86e401122f4813e3bc8
         $this->id = $id;
         $this->name = $name;
         $this->stock = $stock;
@@ -28,7 +20,6 @@ class Product {
         $this->setDescription($description);
     }
 
-<<<<<<< HEAD
     public function getId()
     {
         return $this->id;
@@ -41,27 +32,12 @@ class Product {
 
     public function getStock()
     {
-=======
-    public function getId() {
-        return $this->id;
-    }
-
-    public function getName() {
-        return $this->name;
-    }
-
-    public function getStock() {
->>>>>>> e212dda92d6d7c5779edf86e401122f4813e3bc8
         return $this->stock;
     }
 
     //decrements stock
-<<<<<<< HEAD
     public function boughtStock($bought)
     {
-=======
-    public function boughtStock($bought) {
->>>>>>> e212dda92d6d7c5779edf86e401122f4813e3bc8
         if ($this->stock >= $bought) {
             $this->stock -=$bought;
         } else {
@@ -69,7 +45,6 @@ class Product {
         }
     }
 
-<<<<<<< HEAD
     public function getPrice()
     {
         return $this->price;
@@ -104,36 +79,4 @@ class Product {
     {
         $this->id = $id;
     }
-=======
-    public function getPrice() {
-        return $this->price;
-    }
-
-    public function getDescription() {
-        return $this->description;
-    }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function setStock($stock) {
-        $this->stock = $stock;
-    }
-
-    public function setPrice($price) {
-        $this->price = $price;
-    }
-
-    public function setDescription($string) {
-        $this->description = $string;
-    }
-    
-    public function setId($id) {
-        $this->id = $id;
-    }
-
-
-
->>>>>>> e212dda92d6d7c5779edf86e401122f4813e3bc8
 }
